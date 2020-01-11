e072a290771e1ffa207769de8f2b810c4d0e1549
fatfutor <murat-bark@yandex.ru>
2020-01-11 20:40:04 +0300

    Правки по ишью #3 1-й пункт в процессе

---
+++ source/blocks/checkbox-list/checkbox-list.js
@@ -0,0 +1,17 @@
+export default class CheckboxList {
+  constructor(id) {
+    this.container = document.querySelector(id);
+    if (!this.container) return;
+    this.title = this.container.querySelector('.js-checkbox-list__title');
+    this.list = this.container.querySelector('.js-checkbox-list__list');
+
+    this.title.addEventListener('click', () => {
+      this.title.classList.toggle('checkbox-list__title_close');
+      this.title.classList.toggle('checkbox-list__title_open');
+      this.list.classList.toggle('checkbox-list__list_open');
+    });
+  }
+}
+
+new CheckboxList('#checkbox-first');
+new CheckboxList('#checkbox-second');
--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 // JS
 import './blocks/range/range';
 import './blocks/dropdown/dropdown';
+import './blocks/checkbox-list/checkbox-list';
 import './blocks/filter-dropdown-date/filter-dropdown-date';
 import './blocks/group-dropdown-date/group-dropdown-date';
 import './blocks/dropdown-buttons/dropdown-buttons';
