870f1b792e2ea9061687cc68f8bfffb9786deea7
fatfutor <murat-bark@yandex.ru>
2020-01-29 19:30:27 +0300

    Правки по ишью #4  переписал range в класс

--- source/blocks/range/range.js
+++ source/blocks/range/range.js
@@ -1,15 +1,41 @@
 import 'jquery-ui-dist/jquery-ui.min';
 
-$(() => {
-  $('#slider-range').slider({
-    range: true,
+class Range {
+  constructor(options) {
+    this.nodeId = options.nodeId;
+    this.amountId = options.amountId;
+    this.min = options.min;
+    this.max = options.max;
+    this.values = options.values;
+
+    this._startSlider();
+  }
+
+  _startSlider() {
+    $(() => {
+      const { amountId } = this;
+      $(this.nodeId).slider({
+        range: true,
+        min: this.min,
+        max: this.max,
+        values: this.values,
+        slide(event, ui) {
+          $(amountId).val(`${ui.values[0]}₽ - ${ui.values[1]}₽`);
+        },
+      });
+
+      $(this.amountId).val(`${$(this.nodeId).slider('values', 0)
+      }₽ - ${$(this.nodeId).slider('values', 1)}₽`);
+    });
+  }
+}
+
+new Range(
+  {
+    nodeId: '#slider-range',
+    amountId: '#amount',
     min: 0,
     max: 16500,
     values: [5000, 10000],
-    slide(event, ui) {
-      $('#amount').val(`${ui.values[0]}₽ - ${ui.values[1]}₽`);
-    },
-  });
-  $('#amount').val(`${$('#slider-range').slider('values', 0)
-  }₽ - ${$('#slider-range').slider('values', 1)}₽`);
-});
+  },
+);
